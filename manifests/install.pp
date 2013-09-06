0a635bb10337ec3c51ac67fa0cb7aabd42290f73
Mark Kusch <mark.kusch@gmail.com>
2013-09-06 13:45:05 +0200

    Keep docker installable on Precise for solutionscamp space

--- manifests/install.pp
+++ manifests/install.pp
@@ -13,33 +13,20 @@ class docker::install {
   include apt
   validate_string($docker::version)
   validate_re($::osfamily, '^Debian$', 'This module uses the docker apt repo and only works on Debian systems that support it.')
-  validate_string($::kernelrelease)
 
   apt::source { 'docker':
     location          => 'https://get.docker.io/ubuntu',
     release           => 'docker',
     repos             => 'main',
-    required_packages => 'debian-keyring debian-archive-keyring',
     key               => 'A88D21E9',
     key_source        => 'http://get.docker.io/gpg',
     pin               => '10',
     include_src       => false,
   }
 
-  # determine the package name for 'linux-image-extra-$(uname -r)' based on the
-  # $::kernelrelease fact
-  $kernelpackage = "linux-image-extra-${::kernelrelease}"
-
-  package { $kernelpackage:
-    ensure => present,
-  }
-
   package { 'lxc-docker':
     ensure  => $docker::version,
-    require => [
-        Apt::Source['docker'],
-        Package[$kernelpackage],
-    ],
+    require => Apt::Source['docker'],
   }
 
 }
